4fa02b75083e693f9e74567fca075c7b5b6dc6dc
Esther Stevens <estherstevens@Esthers-MacBook-Pro.local>
2025-04-06 15:01:19 +0100

    add themeing

--- app/src/main/java/stevens/software/scribbledash/ui/theme/Color.kt
+++ app/src/main/java/stevens/software/scribbledash/ui/theme/Color.kt
@@ -2,10 +2,23 @@ package stevens.software.scribbledash.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary = Color(0xFF238CFF)
+val OnPrimary = Color(0xFFFFFFFF)
+val OnPrimaryOpacity = Color(0xFFFFFFa4)
+val Secondary = Color(0xFFAB5CFA)
+val TertiaryContainer = Color(0xFFFA852C)
+val Error = Color(0xFFEF1242)
+val Success = Color(0xFF0DD280)
+val Background = Color(0xFFFEFAF6)
+val OnBackground = Color(0xFF514437)
+val OnSurface = Color(0xFFA5978A)
+val SurfaceContainerHigh = Color(0xFFFFFFFF)
+val SurfaceVariant = Color(0xFFF6F1EC)
+val SurfaceContainerLow = Color(0xFFEEE7E0)
+val SurfaceContainerLowest = Color(0xFFE1D5CA)
+val OnBackgroundVariant = Color(0xFF7F7163)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+
+
+
--- app/src/main/java/stevens/software/scribbledash/ui/theme/Theme.kt
+++ app/src/main/java/stevens/software/scribbledash/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package stevens.software.scribbledash.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,28 +8,39 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Primary,
+    onPrimary = OnPrimary,
+    secondary = Secondary,
+    tertiaryContainer = TertiaryContainer,
+    error = Error,
+    background = Background,
+    onBackground = OnBackground,
+    onSurface = OnSurface,
+    surfaceContainerHigh = SurfaceContainerHigh,
+    surfaceVariant = SurfaceVariant,
+    surfaceContainerLow = SurfaceContainerLow,
+    surfaceContainerLowest = SurfaceContainerLowest
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Primary,
+    onPrimary = OnPrimary,
+    secondary = Secondary,
+    tertiaryContainer = TertiaryContainer,
+    error = Error,
+    background = Background,
+    onBackground = OnBackground,
+    onSurface = OnSurface,
+    surfaceContainerHigh = SurfaceContainerHigh,
+    surfaceVariant = SurfaceVariant,
+    surfaceContainerLow = SurfaceContainerLow,
+    surfaceContainerLowest = SurfaceContainerLowest
 )
 
 @Composable
@@ -50,9 +60,25 @@ fun ScribbleDashTheme(
         else -> LightColorScheme
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    CompositionLocalProvider(localExtendedColors provides extendedColors) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
 }
+
+val localExtendedColors = compositionLocalOf { extendedColors }
+
+data class ExtendedColors(
+    val success: Color,
+    val onPrimaryOpacity: Color,
+    val onBackgroundVariant: Color
+)
+
+val extendedColors = ExtendedColors(
+    success = Success,
+    onPrimaryOpacity = OnPrimaryOpacity,
+    onBackgroundVariant = OnBackgroundVariant
+)
