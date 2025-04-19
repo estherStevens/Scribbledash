881b54561827dc7dca89c2dcff17c240d602437c
Esther Stevens <estherstevens@Esthers-MacBook-Pro.local>
2025-04-19 22:42:09 +0100

    create canvas composable

--- app/src/main/java/stevens/software/scribbledash/DrawingScreen.kt
+++ app/src/main/java/stevens/software/scribbledash/DrawingScreen.kt
@@ -1,12 +1,15 @@
 package stevens.software.scribbledash
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -20,6 +23,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -48,22 +54,55 @@ fun DrawingScreen(
                 .padding(16.dp)
                 .clickable { onNavigateBack() }
         )
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 16.dp)
-            .padding(bottom = 20.dp))
+        Box(
+            modifier = Modifier
+                .padding(horizontal = 29.dp)
+                .padding(bottom = 20.dp)
+            )
         {
-            BottomBar()
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(Modifier.size(53.dp))
+                Text(
+                    text = stringResource(R.string.drawing_title),
+                    style = TextStyle(
+                        fontFamily = bagelFontFamily,
+                        fontWeight = FontWeight.Normal,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        fontSize = 40.sp,
+                    )
+                )
+                Spacer(Modifier.size(32.dp))
+                Canvas(
+                    modifier = Modifier
+                        .shadow(
+                            elevation = 1.dp,
+                            shape = RoundedCornerShape(36.dp)
+                        )
+                        .background(
+                            color = MaterialTheme.colorScheme.surfaceContainerHigh,
+                            shape = RoundedCornerShape(36.dp)
+                        )
+                        .size(360.dp)
+                ) {
+                }
+                Spacer(Modifier.weight(1f))
+                BottomBar()
+            }
+
         }
     }
 }
 
 @Composable
-fun BoxScope.BottomBar(){
-    Row(modifier = Modifier
-        .fillMaxWidth()
-        .align(Alignment.BottomCenter),
-        horizontalArrangement = Arrangement.spacedBy(12.dp)) {
+fun BottomBar() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
         Box(
             modifier = Modifier
                 .background(
@@ -96,22 +135,24 @@ fun BoxScope.BottomBar(){
             )
         }
 
-        Box(modifier = Modifier
-            .background(
-                color = MaterialTheme.colorScheme.surfaceContainerHigh,
-                shape = RoundedCornerShape(20.dp)
-            )
-            .height(64.dp)
-            .weight(2f),
+        Box(
+            modifier = Modifier
+                .background(
+                    color = MaterialTheme.colorScheme.surfaceContainerHigh,
+                    shape = RoundedCornerShape(20.dp)
+                )
+                .height(64.dp)
+                .weight(2f),
             contentAlignment = Alignment.Center
         ) {
-            Box(modifier = Modifier
-                .padding(6.dp)
-                .background(
-                    color = MaterialTheme.extendedColours.surfaceLowest,
-                    shape = RoundedCornerShape(16.dp)
-                )
-                .fillMaxSize(),
+            Box(
+                modifier = Modifier
+                    .padding(6.dp)
+                    .background(
+                        color = MaterialTheme.extendedColours.surfaceLowest,
+                        shape = RoundedCornerShape(16.dp)
+                    )
+                    .fillMaxSize(),
                 contentAlignment = Alignment.Center
             ) {
                 Text(
@@ -131,7 +172,7 @@ fun BoxScope.BottomBar(){
 
 @Preview(showSystemUi = true)
 @Composable
-fun DrawingScreenPreview(){
+fun DrawingScreenPreview() {
     MaterialTheme {
         DrawingScreen(
             onNavigateBack = {}
