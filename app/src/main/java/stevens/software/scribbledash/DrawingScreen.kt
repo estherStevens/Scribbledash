7e7477eb13b4deb9255fe7f03e5b2ff7e4f2c68f
Esther Stevens <estherstevens@Esthers-MacBook-Pro.local>
2025-04-19 21:52:42 +0100

    implement undo, redo and clear canvas button

--- app/src/main/java/stevens/software/scribbledash/DrawingScreen.kt
+++ app/src/main/java/stevens/software/scribbledash/DrawingScreen.kt
@@ -2,18 +2,32 @@ package stevens.software.scribbledash
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import stevens.software.scribbledash.ui.theme.extendedColours
 
 @Composable
 fun DrawingScreen(
@@ -34,7 +48,85 @@ fun DrawingScreen(
                 .padding(16.dp)
                 .clickable { onNavigateBack() }
         )
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+            .padding(bottom = 20.dp))
+        {
+            BottomBar()
+        }
     }
+}
+
+@Composable
+fun BoxScope.BottomBar(){
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .align(Alignment.BottomCenter),
+        horizontalArrangement = Arrangement.spacedBy(12.dp)) {
+        Box(
+            modifier = Modifier
+                .background(
+                    color = MaterialTheme.colorScheme.surfaceContainerLow,
+                    shape = RoundedCornerShape(22.dp)
+                )
+                .size(64.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.undo),
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onBackground
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .background(
+                    color = MaterialTheme.colorScheme.surfaceContainerLow,
+                    shape = RoundedCornerShape(22.dp)
+                )
+                .size(64.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.redo),
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onBackground
+            )
+        }
+
+        Box(modifier = Modifier
+            .background(
+                color = MaterialTheme.colorScheme.surfaceContainerHigh,
+                shape = RoundedCornerShape(20.dp)
+            )
+            .height(64.dp)
+            .weight(2f),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(modifier = Modifier
+                .padding(6.dp)
+                .background(
+                    color = MaterialTheme.extendedColours.surfaceLowest,
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    stringResource(R.string.drawing_clear_canvas).uppercase(),
+                    style = TextStyle(
+                        fontFamily = bagelFontFamily,
+                        fontWeight = FontWeight.Normal,
+                        color = MaterialTheme.colorScheme.onPrimary.copy(0.8f),
+                        fontSize = 18.sp,
+                    )
+                )
+            }
+        }
+    }
+
 }
 
 @Preview(showSystemUi = true)
